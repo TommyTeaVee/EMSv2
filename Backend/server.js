87ed9f1e7dcfcc45dd903e372bd3435ef6262fa1
TommyTeaVee <fihlatv@gmail.com>
2023-12-08 16:38:49 +0200

    Moved admin to middleware auth

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,48 @@
 import express from 'express';
 import cors from 'cors';
+import Jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+app.use('/auth', adminRouter)
+app.use('/employee', EmployeeRouter)
 
 const PORT = 35050;
-const app = express();
 
 
 
 
+
+
+app.get('/', (req, res) =>{
+console.log(`Server online`)
+})
+const app = express() 
 app.use(cors({
     origin: ["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }))
-app.get('/', (req, res) =>{
-console.log(`Server online`)
-})
+app.use(express.json())
+app.use(cookieParser())
+app.use('/auth', adminRouter)
+app.use('/employee', EmployeeRouter)
+app.use(express.static('Public'))
 
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if(token) {
+        Jwt.verify(token, "jwt_secret_key", (err ,decoded) => {
+            if(err) return res.json({Status: false, Error: "Wrong Token"})
+            req.id = decoded.id;
+            req.role = decoded.role;
+            next()
+        })
+    } else {
+        return res.json({Status: false, Error: "Not autheticated"})
+    }
+}
+app.get('/verify',verifyUser, (req, res)=> {
+    return res.json({Status: true, role: req.role, id: req.id})
+} )
 
 app.listen(PORT,()=>{
     console.log(`Server running in Port : ${PORT}`);
