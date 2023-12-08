4cc6648c9ce2b9152465dacea33e8461c456d525
TommyTeaVee <fihlatv@gmail.com>
2023-12-08 15:22:38 +0200

    Database connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,1 +1,20 @@
-//SQL database
+//MYSQL database
+import mysql from 'mysql';
+
+//Database connection string
+const conn= mysql.createConnection({
+    host: 'localhost',
+    user:'sueperadmin',
+    password: '@dmin@dmin1023'
+})
+
+//Database connection verification
+conn.connect(function (err){
+    if(err) {
+        console.log(`Database connection error: ${err.message}`)
+    }
+    else {
+        console.log(`Database connected successfully`)
+    }
+})
+
